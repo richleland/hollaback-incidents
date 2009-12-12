b0fc14758146d3f4ed412c50e8561e1cecf3c83d
richleland <rich@richleland.com>
2009-12-12 17:47:29 -0500

    Added static media stuff.

--- hollaback_incidents/urls.py
+++ hollaback_incidents/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
+from django.conf import settings
 
 admin.autodiscover()
 
@@ -7,3 +8,8 @@ urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     (r'^incidents/', include('incidents.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)', 'django.views.static.serve', { 'document_root': settings.MEDIA_ROOT }),
+    )
