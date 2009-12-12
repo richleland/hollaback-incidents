c3bc71c2911d9f272208fc9f117cf1036d20755a
richleland <rich@richleland.com>
2009-12-12 17:27:22 -0500

    Updated settings to include PROJECT_ROOT.

--- hollaback_incidents/settings.py
+++ hollaback_incidents/settings.py
@@ -1,4 +1,6 @@
-# Django settings for hollaback_incidents project.
+import os.path
+
+PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -66,9 +68,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'hollaback_incidents.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, 'templates'),
 )
 
 INSTALLED_APPS = (
